25bad2a90d9f74cf97acb2597ec06ec7c73694d7
Dmitry Zharikov <zdimon77@gmail.com>
2021-02-01 13:24:51 +0200

    commit from zdimon77@gmail.com ----

--- imagin/game/views/deploy.py
+++ imagin/game/views/deploy.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 import subprocess
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def deploy(request):
     rc = subprocess.call("./deploy", shell=True)
     return HttpResponse('Ok')
